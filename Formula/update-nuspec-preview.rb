698bfebbcc517502a5cc815a86913f7b1e0d83b1
Denis Peshkov <denis.peshkov@outlook.com>
2026-07-14 10:42:40 +0000

    update-nuspec-preview 2.0.144-preview

--- Formula/update-nuspec-preview.rb
+++ Formula/update-nuspec-preview.rb
@@ -1,9 +1,9 @@
 class UpdateNuspecPreview < Formula
   desc "Preview build: sync NuGet dependencies in nuspec files from csproj PackageReference versions"
   homepage "https://github.com/denis-peshkov/update-nuspec-action"
-  version "2.0.123-preview"
-  url "https://github.com/denis-peshkov/update-nuspec-action/archive/4c40415635cbdd342db8a6d290fe23c1dd8953f3.tar.gz"
-  sha256 "09429e850697ddefc3015fc199e7f2dc2cbb81dd43947a9f0b504a6970bfa3ce"
+  version "2.0.144-preview"
+  url "https://github.com/denis-peshkov/update-nuspec-action/archive/9c7a81965feadb5807d6807c704558e04510848e.tar.gz"
+  sha256 "79b99383bdfdd1ad6f2aa71f8107b148486db708ee70a9bbeaf4f77fafb24170"
   license "MIT"
 
   depends_on "rust" => :build
@@ -15,6 +15,6 @@ class UpdateNuspecPreview < Formula
   end
 
   test do
-    assert_match "update-nuspec", shell_output("#{bin}/update-nuspec --version")
+    assert_match version.to_s, shell_output("#{bin}/update-nuspec --version")
   end
 end
